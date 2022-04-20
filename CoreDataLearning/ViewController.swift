b4de90ac8823b6d7f08b70a5093574450fd361a6
Chethana N P <chethana.np@cumulations.com>
2022-04-20 12:38:50 +0530

    Initial Commit

---
+++ CoreDataLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataLearning
+//
+//  Created by Cumulations Technologies Private Limited on 20/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
